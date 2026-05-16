403ad645a46c6e2d56d72ec00fd0dfd0fb8b210d
ryanj06 <ryanjose.0186@gmail.com>
2026-05-15 19:05:16 -0700

    Remove token

--- map.js
+++ map.js
@@ -1,7 +1,7 @@
 import mapboxgl from 'https://cdn.jsdelivr.net/npm/mapbox-gl@2.15.0/+esm';
 import * as d3 from 'https://cdn.jsdelivr.net/npm/d3@7.9.0/+esm';
 
-mapboxgl.accessToken = 'pk.eyJ1IjoicnlhbmowNiIsImEiOiJjbXA3bGs3NncwNGJtMnVxMWdoNXZubzZiIn0.Xj4L1wunBxK5g_FRuxzTow';
+mapboxgl.accessToken = 'YOUR_MAPBOX_TOKEN';
 
 const map = new mapboxgl.Map({
   container: 'map',
